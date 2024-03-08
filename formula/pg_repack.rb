f2687b16677066acb769f831b9255d91589b56f3
Joe Ayers <joseph.ayers@crunchydata.com>
2024-03-08 11:30:52 -0800

    move to pg 14 as 16 works weird

--- formula/pg_repack.rb
+++ formula/pg_repack.rb
@@ -5,20 +5,30 @@ class PgRepack < Formula
   sha256 "9a14d6a95bfa29f856aa10538238622c1f351d38eb350b196c06720a878ccc52"
   license "BSD-3-Clause"
 
-  depends_on "postgresql@16"
+  depends_on "postgresql@14"
 
   bottle do
     root_url "https://github.com/crunchyjoea/homebrew-pg_repack/raw/main/bottle/"
     sha256 arm64_sonoma: "e5d62f282115e1fc901c1b3de792008a0901e3a3b2e0b4c5139a3c888e1f4e0b"
   end
 
+  def postgresql
+    Formula["postgresql@14"]
+  end
+
   def install
+
     system "make", "all"
-    doc.install "COPYRIGHT"
-    doc.install "doc/pgrepack.rst"
-    bin.install "bin/pg_repack"
-    lib.install "lib/pg_repack--1.5.0.sql"
-    lib.install "lib/pg_repack.control"
+    mkdir "stage"
+    system "make", "install", "DESTDIR=#{buildpath}/stage"
+    postgresql_prefix = postgresql.prefix.realpath
+    postgresql_stage_path = File.join("stage", postgresql_prefix)
+    bin.install (buildpath/postgresql_stage_path/"bin").children
+
+    stage_path = File.join("stage", HOMEBREW_PREFIX)
+    lib.install (buildpath/stage_path/"lib").children
+    share.install (buildpath/stage_path/"share").children
+
   end
 
 end
