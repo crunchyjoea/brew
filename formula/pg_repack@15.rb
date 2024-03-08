304a7316dc69d8d873429ac19b17c66dbd3b9fd1
Joe Ayers <joseph.ayers@crunchydata.com>
2024-03-08 14:12:59 -0800

    add 15.rb

---
+++ formula/pg_repack@15.rb
@@ -0,0 +1,34 @@
+class PgRepackAT15 < Formula
+  desc "Reorganize tables in PostgreSQL databases with minimal locks"
+  homepage ""
+  url "https://github.com/reorg/pg_repack/archive/refs/tags/ver_1.5.0.tar.gz"
+  sha256 "9a14d6a95bfa29f856aa10538238622c1f351d38eb350b196c06720a878ccc52"
+  license "BSD-3-Clause"
+
+  depends_on "postgresql@15"
+
+  bottle do
+    root_url "https://github.com/crunchyjoea/homebrew-pg_repack/raw/main/bottle/"
+    sha256 arm64_sonoma: "f5cd776acaab85ab6949df730087de019dcd2c8a6fa4f4455dca8c0d04620573"
+  end
+
+  def postgresql
+    Formula["postgresql@15"]
+  end
+
+  def install
+
+    system "make", "all"
+    mkdir "stage"
+    system "make", "install", "DESTDIR=#{buildpath}/stage"
+    postgresql_prefix = postgresql.prefix.realpath
+    postgresql_stage_path = File.join("stage", postgresql_prefix)
+    bin.install (buildpath/postgresql_stage_path/"bin").children
+
+    stage_path = File.join("stage", "opt/homebrew/opt/postgresql@15/")
+    lib.install (buildpath/stage_path/"lib").children
+    share.install (buildpath/stage_path/"share").children
+
+  end
+
+end
