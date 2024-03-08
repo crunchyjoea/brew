73dd776d62742a6405b093cba30adc1848999b2f
Joe Ayers <joseph.ayers@crunchydata.com>
2024-03-08 11:36:52 -0800

    create new bottle based on pg14

--- formula/pg_repack.rb
+++ formula/pg_repack.rb
@@ -9,7 +9,7 @@ class PgRepack < Formula
 
   bottle do
     root_url "https://github.com/crunchyjoea/homebrew-pg_repack/raw/main/bottle/"
-    sha256 arm64_sonoma: "e5d62f282115e1fc901c1b3de792008a0901e3a3b2e0b4c5139a3c888e1f4e0b"
+    sha256 arm64_sonoma: "f5cd776acaab85ab6949df730087de019dcd2c8a6fa4f4455dca8c0d04620573"
   end
 
   def postgresql
